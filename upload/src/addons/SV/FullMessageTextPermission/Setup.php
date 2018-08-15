c00c4dd1a9f5839ec4304e6aad5242fc5ae35820
Xon <tazipper1@hotmail.com>
2018-08-15 22:14:55 +0800

    Fixup installer

--- upload/src/addons/SV/FullMessageTextPermission/Setup.php
+++ upload/src/addons/SV/FullMessageTextPermission/Setup.php
@@ -11,24 +11,100 @@ namespace SV\FullMessageTextPermission;
 
 use XF\AddOn\AbstractSetup;
 use XF\Db\Schema\Alter;
+use XF\Db\Schema\Create;
 
 class Setup extends AbstractSetup
 {
-    public function install(array $stepParams = [])
+    use StepRunnerInstallTrait;
+    use StepRunnerUpgradeTrait;
+    use StepRunnerUninstallTrait;
+
+    public function installStep1()
     {
         $this->schemaManager()->alterTable('xf_user_option', function (Alter $table) {
-            $table->addColumn('fmp_always_email_notify', 'tinyint', 3)->setDefault(0);
+            $this->addOrChangeColumn($table, 'fmp_always_email_notify', 'tinyint', 3)->setDefault(0);
         });
     }
 
-    public function upgrade(array $stepParams = [])
+    public function installStep2()
     {
+        $this->applyRegistrationDefaults([
+            'fmp_always_email_notify' => 0,
+        ]);
     }
 
-    public function uninstall(array $stepParams = [])
+    public function upgrade2000200Step1()
+    {
+        $this->installStep1();
+    }
+
+    public function uninstallStep1()
     {
         $this->schemaManager()->alterTable('xf_user_option', function (Alter $table) {
             $table->dropColumns('fmp_always_email_notify');
         });
+    }
+
+    /**
+     * @param array $newRegistrationDefaults
+     */
+    protected function applyRegistrationDefaults(array $newRegistrationDefaults)
+    {
+        /** @var \XF\Entity\Option $option */
+        $option = $this->app->finder('XF:Option')
+                            ->where('option_id', '=', 'registrationDefaults')
+                            ->fetchOne();
+
+        if (!$option)
+        {
+            // Option: Mr. XenForo I don't feel so good
+            throw new \LogicException("XenForo installation is damaged. Expected option 'registrationDefaults' to exist.");
+        }
+        $registrationDefaults = $option->option_value;
+
+        foreach ($newRegistrationDefaults AS $optionName => $optionDefault)
+        {
+            if (!isset($registrationDefaults[$optionName]))
+            {
+                $registrationDefaults[$optionName] = $optionDefault;
+            }
+        }
+
+        $option->option_value = $registrationDefaults;
+        $option->saveIfChanged();
+    }
+
+    /**
+     * @param Create|Alter $table
+     * @param string       $name
+     * @param string|null  $type
+     * @param string|null  $length
+     *
+     * @return \XF\Db\Schema\Column
+     */
+    protected function addOrChangeColumn($table, $name, $type = null, $length = null)
+    {
+        if ($table instanceof Create)
+        {
+            $table->checkExists(true);
+
+            return $table->addColumn($name, $type, $length);
+        }
+        else
+        {
+            if ($table instanceof Alter)
+            {
+                if ($table->getColumnDefinition($name))
+                {
+                    return $table->changeColumn($name, $type, $length);
+                }
+
+                return $table->addColumn($name, $type, $length);
+            }
+            else
+            {
+                throw new \LogicException("Unknown schema DDL type " . get_class($table));
+            }
+        }
     }
 }
