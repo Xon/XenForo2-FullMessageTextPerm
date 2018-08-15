d0e05cc3dc7e731a5a340c5d43ff87437fa39293
Xon <tazipper1@hotmail.com>
2018-08-15 22:15:19 +0800

    Fixup installer take #2

--- upload/src/addons/SV/FullMessageTextPermission/Setup.php
+++ upload/src/addons/SV/FullMessageTextPermission/Setup.php
@@ -10,6 +10,9 @@
 namespace SV\FullMessageTextPermission;
 
 use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
 use XF\Db\Schema\Alter;
 use XF\Db\Schema\Create;
 
