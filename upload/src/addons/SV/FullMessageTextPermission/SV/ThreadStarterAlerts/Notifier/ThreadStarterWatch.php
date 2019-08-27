a154982f0f7f96dd6efc2a5eb98e149f5c8fa3c0
Xon <tazipper1@hotmail.com>
2019-08-28 07:08:00 +0800

    Thread Starter Alerts v2.1.0+ compatibility

--- upload/src/addons/SV/FullMessageTextPermission/SV/ThreadStarterAlerts/Notifier/ThreadStarterWatch.php
+++ upload/src/addons/SV/FullMessageTextPermission/SV/ThreadStarterAlerts/Notifier/ThreadStarterWatch.php
@@ -11,8 +11,9 @@ class ThreadStarterWatch extends XFCP_ThreadStarterWatch
 {
     public function sendEmail(User $user)
     {
+        $type = ($this->actionType === 'thread' ? 'forum_thread' : 'thread_reply');
         /** @var \SV\FullMessageTextPermission\XF\Entity\User $user */
-        $user->configureFullEmailMessageContent('thread_' . $this->actionType, $this->post->Thread->node_id);
+        $user->configureFullEmailMessageContent($type, $this->post->Thread->node_id);
 
         return parent::sendEmail($user);
     }
